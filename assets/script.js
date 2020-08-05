0edb186da54ab371e42142dce7e4b37b3a42ebaf
a-thread <aiden.threadgoode@gmail.com>
2020-08-05 00:57:51 -0400

    updating with new look

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
       const lat = response.coord.lat; // latitude
       const lon = response.coord.lon; // longitude
 
-      $("h3.city-title").text(response.name); // displaying city name
+      $("h2.city-title").text(response.name); // displaying city name
       $("h5.date").text(momentDate); // displaying date
 
       // setting icon on title page
@@ -38,17 +38,17 @@ $(document).ready(function () {
       "&apikey=955452fde6d16eea4b0e62b34551cd90&units=imperial";
 
     $.get(queryUrl).then(function (response) {
-      var tempEl = $("<h6>") // current temp
-        .addClass("city-temp")
+      var tempEl = $("<li>") // current temp
+        .addClass("city-temp collection-item")
         .text("Temperature: " + response.current.temp + "° F");
-      var humEl = $("<h6>") // current humidity
-        .addClass("city-hum")
+      var humEl = $("<li>") // current humidity
+        .addClass("city-hum collection-item")
         .text("Humidity: " + response.current.humidity + "%");
-      var windEl = $("<h6>") // current wind speed
-        .addClass("city-wind")
+      var windEl = $("<li>") // current wind speed
+        .addClass("city-wind collection-item")
         .text("Wind Speed: " + response.current.wind_speed + "MPH"); // wind speed
       var uviData = response.current.uvi; // current UVI data
-      var uviTitle = $("<h6>").addClass("city-uviTitle").text("UV Index:  "); // UVI label
+      var uviTitle = $("<li>").addClass("city-uviTitle").text("UV Index:  "); // UVI label
       var uviBtn = $("<a>").addClass("btn-small").text(uviData); // UVI button
 
       // setting color of UVI btn according to sun safety guidelines
@@ -64,20 +64,20 @@ $(document).ready(function () {
       }
 
       // appending current weather data elements to title card
-      $(".location-specs")
+      $(".location-specs1")
         .append(tempEl, humEl, windEl)
         .append(uviTitle.append(uviBtn));
 
       // looping through the 5 day forecast
       for (var i = 1; i < 6; i++) {
-        var time = moment.unix(response.daily[i].dt).format("ddd"); // getting date from API and setting format
+        var time = moment.unix(response.daily[i].dt).format("dddd"); // getting date from API and setting format
 
         var card = $("<div>").addClass("card"); // creating card
-        var cardTitle = $("<h7>").addClass("card-title header").text(time); // setting title
+        var cardTitle = $("<h4>").addClass("card-title header").text(time); // setting title
 
         // getting & setting icon code
         var cardIcon = $("<img>")
-          .addClass("card-icon")
+          .addClass("card-icon row")
           .attr("src", [
             "http://openweathermap.org/img/w/" +
               response.daily[i].weather[0].icon +
@@ -86,13 +86,13 @@ $(document).ready(function () {
           .attr("alt", response.daily[i].weather[0].description);
 
         // card temp
-        var cardTemp = $("<p>")
-          .addClass("card-temp")
+        var cardTemp = $("<h6>")
+          .addClass("card-temp row")
           .text("Temp: " + response.daily[i].temp.day + "° F");
 
         // card humidity
-        var cardHum = $("<p>")
-          .addClass("card-temp")
+        var cardHum = $("<h6>")
+          .addClass("card-hum row")
           .text("Humidity: " + response.daily[i].humidity + "%");
 
         // appending card w/ elements
@@ -110,11 +110,9 @@ $(document).ready(function () {
 
   // function to make buttons beneath searchbar
   function makeRow(text) {
-    var li = $("<a>")
-      .addClass("collection-item history")
-      .attr("role", "link")
-      .text(text);
-    $(".collection").prepend(li);
+    var a = $("<a>").addClass("history").attr("role", "link").text(text);
+    var li = $("<li>").append(a);
+    $("ul.dropdown-content").prepend(li);
   }
 
   // preventing anything other than numbers to be allowed in the textbox
@@ -130,8 +128,8 @@ $(document).ready(function () {
   }
 
   // search button listener
-  $(".searchBtn").on("click", function () {
-    var zipInput = $("#city").val(); // getting search input
+  $("button.searchBtn").on("click", function () {
+    var zipInput = $("#search").val(); // getting search input
 
     if (zipInput === "") {
       alert("must enter a zip code");
@@ -145,15 +143,18 @@ $(document).ready(function () {
 
     getCoordinates(zipInput); // getting lat & lon from zipcode
 
-    $("#city").val(""); // clearing input text
+    $("#search").val(""); // clearing input text
   });
 
   // recent search links
-  $("a.history").on("click", function () {
+  $(document).on("click", "a.history", function (event) {
     $("#forecast").empty(); // clearing previous forecast cards
     $(".location-specs").empty(); // clearing previous title card
     getCoordinates($(this).text()); // displaying new forecast & title card
   });
+
+  // recent search drop down
+  $('.dropdown-trigger').dropdown();
 
   // displaying last searched city
   getCoordinates(history[history.length - 1]);
